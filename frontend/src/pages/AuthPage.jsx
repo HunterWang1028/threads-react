452cd24008b1eac523b8e10cc6e23dd8be53e398
Hunter Wang <s092284s@gmail.com>
2024-09-24 21:41:35 +0800

    Login & Signup function (Recoil for state control)

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,13 @@
+import { useRecoilState } from "recoil";
+import SignupCard from "../components/SignupCard";
+import authScreenAtom from "../atoms/authAtom";
+import LoginCard from "../components/LoginCard";
+
+const AuthPage = () => {
+  const authScreenState = useRecoilState(authScreenAtom);
+  console.log(authScreenState);
+
+  return <>{authScreenState[0] === "login" ? <LoginCard /> : <SignupCard />}</>;
+};
+
+export default AuthPage;
