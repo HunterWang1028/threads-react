4f8724e7bbd53fcce1bf04b3cd60eb50fc9bbcff
Hunter Wang <s092284s@gmail.com>
2024-10-13 04:52:30 +0800

    back to before oauth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,6 @@
 import express from "express";
-import passport from "passport";
 import {
   followUnfollowUser,
-  getGoogleUser,
   getSuggestedUsers,
   getUserProfile,
   loginUser,
@@ -11,7 +9,6 @@ import {
   updateUser,
 } from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
-import generateTokenAndSetCookie from "../utils/generateTokenAndSetCookie.js";
 
 const router = express.Router();
 
